9798c647571ac5a4fc4ddb24b2317ceff3977180
gorou12 <dummy@example.com>
2025-02-05 23:25:15 +0900

    add footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
+        <footer>&copy; 2025 gorou12 All rights reserved.</footer>
       </body>
     </html>
   );
